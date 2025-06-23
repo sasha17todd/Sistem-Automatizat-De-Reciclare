a11d2e214838548a497670c6a5aeee4b126996d1
sasha17todd <146314986+sasha17todd@users.noreply.github.com>
2025-06-23 21:14:18 +0300

    Add files via upload
    Test Senzor IR + Stepper + Servo

---
+++ Test_IR_Stepper_Servo.ino
@@ -0,0 +1,63 @@
+/*Test IR + Stepper + Servo*/
+
+#include <Stepper.h>
+#include <ESP32Servo.h>
+
+const int PIN_IR = 33;
+const int STEPS = 2048;
+Stepper stepper(STEPS, 25, 27, 26, 14);
+int positionDegree = 0;
+
+Servo mg;
+const int PIN_SERVO = 13;
+const int ANG_MAX = 150;
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(PIN_IR, INPUT);
+  stepper.setSpeed(10);
+
+  mg.setPeriodHertz(50);
+  mg.attach(PIN_SERVO, 500, 2400);
+  mg.write(0);
+}
+
+void loop() {
+  static bool detectat = false;
+
+  int ir = digitalRead(PIN_IR);
+
+  if (ir == LOW && !detectat) {
+    detectat = true;
+    Serial.println("Obiect detectat!");
+
+    rotireLa(90);
+    delay(2000);
+    sweepServo();
+    rotireLa(0);
+  }
+
+  if (ir == HIGH && detectat) {
+    detectat = false;
+  }
+
+  delay(100);
+}
+
+void rotireLa(int target) {
+  int diff = ((target - positionDegree + 540) % 360) - 180;
+  int steps = diff * STEPS / 360;
+  stepper.step(steps);
+  positionDegree = target;
+
+  Serial.printf("Rotație la %d° (pași %d)\n", positionDegree, steps);
+}
+
+void sweepServo() {
+  mg.write(0);
+  delay(200);
+  mg.write(ANG_MAX);
+  delay(800);
+  mg.write(0);
+  delay(200);
+}
